90724d4dffb01be41ecdeec08ead3b2cb158eed9
Rory Doherty <rory@tigera.io>
2022-11-24 18:51:49 +0000

    Don't use artifacts for clone

--- .argoci/scripts/global_prologue.sh
+++ .argoci/scripts/global_prologue.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 set -x
 chmod a+w ${CI_HOME}
-chmod a+w ${CI_HOME}/${CI_GIT_DIR}
 
 echo "[INFO] generate a default id_rsa"
 ssh-keygen -t rsa -b 4096 -N '' -f ~/.ssh/id_rsa
@@ -14,6 +13,11 @@ printenv marvin > ~/.keys/marvin
 chmod 0600 ~/.keys/*
 ssh-add ~/.keys/*
 ssh-keyscan github.com >> /root/.ssh/known_hosts
+
+# clone repo and checkout branch
+git clone ${CI_GIT_REPO_URL}
+git checkout ${CI_GIT_CLONED_REPO}
+chmod a+w ${CI_HOME}/${CI_GIT_DIR}
 
 export REPO_DIR="$(pwd)"
 mkdir artifacts
