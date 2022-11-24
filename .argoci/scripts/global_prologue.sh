2280941dff5fcdd5eeba6b40ef69558a56d1415d
Rory Doherty <rory@tigera.io>
2022-11-24 11:27:42 +0000

    Use a volume for /src dir

--- .argoci/scripts/global_prologue.sh
+++ .argoci/scripts/global_prologue.sh
@@ -1,5 +1,13 @@
 #! /bin/bash -x
 
+eval `ssh-agent -s`
+mkdir -p /root/.ssh
+touch /root/.ssh/known_hosts
+# Copy secrets from mounted directory to home
+chmod 0600 ~/.keys/*
+ssh-add ~/.keys/*
+ssh-keyscan github.com >> /root/.ssh/known_hosts
+
 export REPO_DIR="$(pwd)"
 mkdir artifacts
 echo $DOCKERHUB_PASSWORD | docker login --username "$DOCKERHUB_USERNAME" --password-stdin
