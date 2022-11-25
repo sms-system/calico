5b9d015245cabdbd078e50b585d2b8ea27fbb6fa
Rory Doherty <rory@tigera.io>
2022-11-25 14:04:39 +0000

    fixups

--- .argoci/scripts/global_prologue.sh
+++ .argoci/scripts/global_prologue.sh
@@ -5,6 +5,3 @@ chmod a+w ${CI_HOME}
 export REPO_DIR="$(pwd)"
 mkdir artifacts
 echo $DOCKERHUB_PASSWORD | docker login --username "$DOCKERHUB_USERNAME" --password-stdin
-
-# Enable ipv6 in pod
-sysctl net.ipv6.conf.all.disable_ipv6=0
